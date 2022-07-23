9d725bf7d00165574f0623b6e09d4535da37e472
Eric Freudenthal <eric@freudenthal.net>
2022-07-22 22:18:31 -0600

    moved all control into interrupt handlers

--- wakedemo/wakedemo.c
+++ wakedemo/wakedemo.c
@@ -14,6 +14,9 @@
 
 #define SWITCHES 15
 
+char blue = 31, green = 0, red = 31;
+unsigned char step = 0;
+
 static char 
 switch_update_interrupt_sense()
 {
@@ -45,8 +48,31 @@ switch_interrupt_handler()
 
 
 // axis zero for col, axis 1 for row
-short drawPos[2] = {10,10}, controlPos[2] = {10,10};
-short velocity[2] = {3,8}, limits[2] = {screenWidth-36, screenHeight-8};
+
+short drawPos[2] = {1,10}, controlPos[2] = {2, 10};
+short colVelocity = 1, colLimits[2] = {1, screenWidth/2};
+
+void
+draw_ball(int col, int row, unsigned short color)
+{
+  fillRectangle(col-1, row-1, 3, 3, color);
+}
+
+
+void
+screen_update_ball()
+{
+  for (char axis = 0; axis < 2; axis ++) 
+    if (drawPos[axis] != controlPos[axis]) /* position changed? */
+      goto redraw;
+  return;			/* nothing to do */
+ redraw:
+  draw_ball(drawPos[0], drawPos[1], COLOR_BLUE); /* erase */
+  for (char axis = 0; axis < 2; axis ++) 
+    drawPos[axis] = controlPos[axis];
+  draw_ball(drawPos[0], drawPos[1], COLOR_WHITE); /* draw */
+}
+  
 
 short redrawScreen = 1;
 u_int controlFontColor = COLOR_GREEN;
@@ -57,7 +83,27 @@ void wdt_c_handler()
 
   secCount ++;
   if (secCount >= 25) {		/* 10/sec */
-    secCount = 0;
+   
+    {				/* move ball */
+      short oldCol = controlPos[0];
+      short newCol = oldCol + colVelocity;
+      if (newCol <= colLimits[0] || newCol >= colLimits[1])
+	colVelocity = -colVelocity;
+      else
+	controlPos[0] = newCol;
+    }
+
+    {				/* update hourglass */
+      if (switches & SW3) green = (green + 1) % 64;
+      if (switches & SW2) blue = (blue + 2) % 32;
+      if (switches & SW1) red = (red - 3) % 32;
+      if (step <= 30)
+	step ++;
+      else
+	step = 0;
+      secCount = 0;
+    }
+    if (switches & SW4) return;
     redrawScreen = 1;
   }
 }
@@ -88,35 +134,41 @@ void main()
   }
 }
 
-    
+void
+screen_update_hourglass()
+{
+  static unsigned char row = screenHeight / 2, col = screenWidth / 2;
+  static char lastStep = 0;
+  
+  if (step == 0 || (lastStep > step)) {
+    clearScreen(COLOR_BLUE);
+    lastStep = 0;
+  } else {
+    for (; lastStep <= step; lastStep++) {
+      int startCol = col - lastStep;
+      int endCol = col + lastStep;
+      int width = 1 + endCol - startCol;
+      
+      // a color in this BGR encoding is BBBB BGGG GGGR RRRR
+      unsigned int color = (blue << 11) | (green << 5) | red;
+      
+      fillRectangle(startCol, row+lastStep, width, 1, color);
+      fillRectangle(startCol, row-lastStep, width, 1, color);
+    }
+  }
+}  
+
+
     
 void
 update_shape()
 {
-  static unsigned char row = screenHeight / 2, col = screenWidth / 2;
-  static char blue = 31, green = 0, red = 31;
-  static unsigned char step = 0;
-  if (switches & SW4) return;
-  if (step <= 60) {
-    int startCol = col - step;
-    int endCol = col + step;
-    int width = 1 + endCol - startCol;
-    // a color in this BGR encoding is BBBB BGGG GGGR RRRR
-    unsigned int color = (blue << 11) | (green << 5) | red;
-    fillRectangle(startCol, row+step, width, 1, color);
-    fillRectangle(startCol, row-step, width, 1, color);
-    if (switches & SW3) green = (green + 1) % 64;
-    if (switches & SW2) blue = (blue + 2) % 32;
-    if (switches & SW1) red = (red - 3) % 32;
-    step ++;
-  } else {
-     clearScreen(COLOR_BLUE);
-     step = 0;
-  }
+  screen_update_ball();
+  screen_update_hourglass();
 }
+   
 
 
-/* Switch on S2 */
 void
 __interrupt_vec(PORT2_VECTOR) Port_2(){
   if (P2IFG & SWITCHES) {	      /* did a button cause this interrupt? */
